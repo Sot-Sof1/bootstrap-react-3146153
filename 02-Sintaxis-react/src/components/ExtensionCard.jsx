4beaacb47cd4edb7922e649c02ee0f49b1c6d8ab
Sot-Sof1 <sofiasotelo345@gmail.com>
2025-11-04 13:28:33 -0500

    Avances estilos Extensions

--- 02-Sintaxis-react/src/components/ExtensionCard.jsx
+++ 02-Sintaxis-react/src/components/ExtensionCard.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import "../stylesheets/ExtensionCard.css"
+
 
 export const ExtensionCard = () => {
   return (
-    <div>ExtensionCard</div>
+    <>
+      <div className="contenedor">
+        <div className="contenedor-2">
+          <img src="./public/logo-devlens.svg" alt="" />
+          <div className="contenedor-textos">
+            <h2>DevLens</h2>
+            <p>Quickly inspect page layouts and visualize element boundaries.</p>
+          </div>
+        </div>
+
+         <div className="contenedor-btn">
+          <button>Remove</button>
+         </div>
+      </div>
+    </>
   )
 }
