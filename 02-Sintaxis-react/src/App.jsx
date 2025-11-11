fc5ce5c09ff3dbee660252b26e633877ef313b48
Sot-Sof1 <sofiasotelo345@gmail.com>
2025-11-11 16:54:57 -0500

    Cartas terminadas

--- 02-Sintaxis-react/src/App.jsx
+++ 02-Sintaxis-react/src/App.jsx
@@ -9,7 +9,7 @@ export const App = () => {
   return (
     <>
       <NavBar />
-      
+
       <div className='contenedorCards'>
 
 
@@ -70,10 +70,21 @@ export const App = () => {
       </div>
 
 
-      <LoginForm/>
+      <LoginForm />
 
-      <ProductCard/>
-
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-6 col-lg-4">
+            <ProductCard />
+          </div>
+          <div className="col-12 col-md-6 col-lg-4 mt-4 mt-md-0">
+            <ProductCard />
+          </div>
+          <div className="col-12 col-md-12 col-lg-4 mt-4 mt-lg-0">
+            <ProductCard />
+          </div>
+        </div>
+      </div>
 
     </>
   )
