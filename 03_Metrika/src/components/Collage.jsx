dc81627437243030cb3385ea8e2eae6549edc98b
Sot-Sof1 <sofiasotelo345@gmail.com>
2025-11-20 13:32:58 -0500

    Metrika terminado

--- 03_Metrika/src/components/Collage.jsx
+++ 03_Metrika/src/components/Collage.jsx
@@ -4,15 +4,48 @@ import "../styleheets/collage.css"
 export const Collage = () => {
   return (
     <>
-      <div className="contenedor">
-        <div className="contenedor-1" style={{width: "100%"}}>
-          <div className="cuadro1  bg-primary" style={{width: "80px", height: "80px"}}> About Us </div>
-          <div className="cuadro2  bg-success" style={{width: "80px", height: "80px"}}> About Us </div>
-          <div className="cuadro3  bg-danger" style={{width: "80px", height: "80px"}}> About Us </div>
+      <div class="container text-white fon">
+        <div class="row align-items-center">
+          <div class="col">
+            <div className='display'>
+              <div className="bg-verdeagua">
+                <img className='imagen-1' src="./public/libro.png" alt="" />
+                <p className='texto-1'>About Us</p>
+              </div>
+              <div className='bg-verde'>
+                <img className='imagen-2' src="./public/archivos.png" alt="" />
+                <p className='texto-2'>Our Team</p>
+              </div>
+            </div>
+            <div className='caja-sola bg-rojo'>
+              <img className='imagen-3' src="./public/descarga.png" alt="" />
+              <p className='texto-3'>Resume</p>
+            </div>
+          </div>
+          <div class="col caja-grande bg-azul">
+            <img className='collage-fotos' src="./public/Collage.png" alt="" />
+            <h1>Our Works</h1>
+          </div>
+          <div class="col">
+            <div className='caja-sola-2 bg-rosado'>
+              <img className='imagen-4' src="./public/mapa.png" alt="" />
+              <p className='texto-4'>Contacts</p>
+            </div>
+            <div className='display'>
+              <div className='bg-bronce'>
+                <img className='imagen-5' src="./public/lapiz.png" alt="" />
+                <p className='texto-5'>Blog</p>
+              </div>
+              <div className='bg-naranja'>
+                <img className='imagen-6' src="./public/sobre.png" alt="" />
+                <p className='texto-6'>Freedback</p>
+              </div>
+            </div>
+
+          </div>
         </div>
-        <div className="contenedor-2"></div>
-        <div className="contenedor-3"></div>
       </div>
+
     </>
   )
 }
